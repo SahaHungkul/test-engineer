7eebc523e39699264c00ea26434a76e49f1756da
SahaHungkul <mhasbysyabani27@gmail.com>
2025-10-23 09:55:51 +0700

    first push

---
+++ app/Models/ChartOfAccount.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChartOfAccount extends Model
+{
+    use HasFactory;
+
+    protected $table = 'chart_of_accounts';
+
+    protected $fillable = [
+        'kode',
+        'nama',
+        'kategori',
+    ];
+}
